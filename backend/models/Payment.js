58a2934874b1a33678b7bfab04a7d9fc86e60867
codecraft26 <codescraft26@gmail.com>
2022-09-28 19:44:40 +0530

    making model for payment

---
+++ backend/models/Payment.js
@@ -0,0 +1,23 @@
+const mongoose =require("mongoose");
+
+const schema = new mongoose.Schema({
+  razorpay_order_id: {
+    type: String,
+    required: true,
+  },
+  razorpay_payment_id: {
+    type: String,
+    required: true,
+  },
+  razorpay_signature: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports=mongoose.model("Payment",schema)
